c3c4494ad17b4872c37c5e26d8997af323ce24d1
nwin19 <nandarwinbuma@gmail.com>
2024-02-11 20:35:56 +0630

    Day - 1 - 1

--- DotNetTrainingBatch3.ConsoleApp/Program.cs
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
+
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
@@ -15,32 +15,46 @@ Console.WriteLine("Hello, World!");
 
 // Ctrl + .
 // checking line by line => f11
+
+// UI, BL, DA
+
+// CRUD
+
+// ntier three tier
+
+// In BL (Business Logic)
+// Kpay
+// Transfer
+// from mobile no
+// to mobile no
+// amount
+// passcode
+
+// In DA (Data Access)
+// mobile no (-1000)
+// mobile no (+1000)
+// from to +1000 date
+
+
+// We will start to learn how to communiate DA and SQL 
+
 SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = @"DESKTOP-O796GIE\SQLEXPRESS";
 sqlConnectionStringBuilder.InitialCatalog = "TestDb1";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "qqq12";
 
-string query = "select * from tb_blog";
-SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString); // build a connection
-sqlConnection.Open();
+Console.WriteLine("Connection: " + sqlConnectionStringBuilder.ConnectionString);
 
-SqlCommand cmd = new SqlCommand(query,sqlConnection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+Console.WriteLine("Connection Opening........");
+connection.Open();
+Console.WriteLine("Connection Opend!");
 
-DataTable dt = new DataTable();
-adapter.Fill(dt);
-sqlConnection.Close();
+Console.WriteLine("Connection Closing......");
+connection.Close();
+Console.WriteLine("Connection Closed!");
 
-// dataset
-// datatable
-// Datarow
-// DataColumn
-
-foreach(DataRow dr in dt.Rows)
-{
-    Console.WriteLine(dr["BlogId"]);
-    Console.WriteLine(dr["BlogTitle"]);
-    Console.WriteLine(dr["BlogAuthor"]);
-    Console.WriteLine(dr["BlogContent"]);
-}
 
 
 Console.ReadKey();
